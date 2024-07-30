0a76d730259d80e3d8686b126c655852e3ba21fd
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-07-29 22:42:49 -0500

    removing next

--- pantry/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xvryZNT94vFEz9zs1dcDUZXL72KELmLapmlcuY4qciI=\"\n}"
--- pantry/firebase.js
+++ pantry/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnUxbg6hgtfoT62fE9VtHBRb8Yx1gBhzo",
-  authDomain: "pantryapp-8388d.firebaseapp.com",
-  projectId: "pantryapp-8388d",
-  storageBucket: "pantryapp-8388d.appspot.com",
-  messagingSenderId: "385418212187",
-  appId: "1:385418212187:web:74fee876c9dc1835aa207e",
-  measurementId: "G-NGF9X03485"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
